fd84aeb05b2e53f1770241ca73f930627d498a01
Cristina-Lue <aracelylue5@gmail.com>
2024-07-26 13:34:07 -0600

    Add files via upload

---
+++ encriptacion/encriptacion/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace encriptacion
+{
+    partial class frmCesar
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbltitulo = new System.Windows.Forms.Label();
+            this.lbltexto = new System.Windows.Forms.Label();
+            this.lblllave = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtLlave = new System.Windows.Forms.TextBox();
+            this.txtTexto = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.grpOpcion = new System.Windows.Forms.GroupBox();
+            this.rbdEncriptar = new System.Windows.Forms.RadioButton();
+            this.rbdDesencriptar = new System.Windows.Forms.RadioButton();
+            this.btnEncriptar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.grpOpcion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbltitulo
+            // 
+            this.lbltitulo.AutoSize = true;
+            this.lbltitulo.Location = new System.Drawing.Point(167, 9);
+            this.lbltitulo.Name = "lbltitulo";
+            this.lbltitulo.Size = new System.Drawing.Size(160, 13);
+            this.lbltitulo.TabIndex = 0;
+            this.lbltitulo.Text = "Encriptamiento por código César";
+            // 
+            // lbltexto
+            // 
+            this.lbltexto.AutoSize = true;
+            this.lbltexto.Location = new System.Drawing.Point(2, 48);
+            this.lbltexto.Name = "lbltexto";
+            this.lbltexto.Size = new System.Drawing.Size(201, 13);
+            this.lbltexto.TabIndex = 2;
+            this.lbltexto.Text = "Ingrese el texto a Encriptar/ Desencriptar";
+            // 
+            // lblllave
+            // 
+            this.lblllave.AutoSize = true;
+            this.lblllave.Location = new System.Drawing.Point(248, 48);
+            this.lblllave.Name = "lblllave";
+            this.lblllave.Size = new System.Drawing.Size(152, 13);
+            this.lblllave.TabIndex = 3;
+            this.lblllave.Text = "Llave(Digite un número entero)";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(55, 238);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 4;
+            // 
+            // txtLlave
+            // 
+            this.txtLlave.Location = new System.Drawing.Point(406, 45);
+            this.txtLlave.Name = "txtLlave";
+            this.txtLlave.Size = new System.Drawing.Size(100, 20);
+            this.txtLlave.TabIndex = 5;
+            this.txtLlave.TextChanged += new System.EventHandler(this.txtLlave_TextChanged);
+            // 
+            // txtTexto
+            // 
+            this.txtTexto.Location = new System.Drawing.Point(29, 64);
+            this.txtTexto.Multiline = true;
+            this.txtTexto.Name = "txtTexto";
+            this.txtTexto.Size = new System.Drawing.Size(371, 79);
+            this.txtTexto.TabIndex = 6;
+            this.txtTexto.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(58, 254);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(330, 67);
+            this.txtResultado.TabIndex = 7;
+            this.txtResultado.TextChanged += new System.EventHandler(this.txtResultado_TextChanged);
+            // 
+            // grpOpcion
+            // 
+            this.grpOpcion.Controls.Add(this.rbdEncriptar);
+            this.grpOpcion.Controls.Add(this.rbdDesencriptar);
+            this.grpOpcion.Location = new System.Drawing.Point(39, 152);
+            this.grpOpcion.Name = "grpOpcion";
+            this.grpOpcion.Size = new System.Drawing.Size(254, 72);
+            this.grpOpcion.TabIndex = 8;
+            this.grpOpcion.TabStop = false;
+            this.grpOpcion.Text = "Seleccione una opción";
+            // 
+            // rbdEncriptar
+            // 
+            this.rbdEncriptar.AutoSize = true;
+            this.rbdEncriptar.Checked = true;
+            this.rbdEncriptar.Location = new System.Drawing.Point(19, 38);
+            this.rbdEncriptar.Name = "rbdEncriptar";
+            this.rbdEncriptar.Size = new System.Drawing.Size(67, 17);
+            this.rbdEncriptar.TabIndex = 0;
+            this.rbdEncriptar.TabStop = true;
+            this.rbdEncriptar.Text = "Encriptar";
+            this.rbdEncriptar.UseVisualStyleBackColor = true;
+            this.rbdEncriptar.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rbdDesencriptar
+            // 
+            this.rbdDesencriptar.AutoSize = true;
+            this.rbdDesencriptar.Location = new System.Drawing.Point(131, 38);
+            this.rbdDesencriptar.Name = "rbdDesencriptar";
+            this.rbdDesencriptar.Size = new System.Drawing.Size(85, 17);
+            this.rbdDesencriptar.TabIndex = 9;
+            this.rbdDesencriptar.Text = "Desencriptar";
+            this.rbdDesencriptar.UseVisualStyleBackColor = true;
+            this.rbdDesencriptar.CheckedChanged += new System.EventHandler(this.rbdDesencriptar_CheckedChanged);
+            // 
+            // btnEncriptar
+            // 
+            this.btnEncriptar.Location = new System.Drawing.Point(335, 161);
+            this.btnEncriptar.Name = "btnEncriptar";
+            this.btnEncriptar.Size = new System.Drawing.Size(75, 23);
+            this.btnEncriptar.TabIndex = 10;
+            this.btnEncriptar.Text = "&Encriptar";
+            this.btnEncriptar.UseVisualStyleBackColor = true;
+            this.btnEncriptar.Click += new System.EventHandler(this.btnEncriptar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(335, 190);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 11;
+            this.btnLimpiar.Text = "&Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(335, 219);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 12;
+            this.btnSalir.Text = "&Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmCesar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 333);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEncriptar);
+            this.Controls.Add(this.grpOpcion);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtTexto);
+            this.Controls.Add(this.txtLlave);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblllave);
+            this.Controls.Add(this.lbltexto);
+            this.Controls.Add(this.lbltitulo);
+            this.Name = "frmCesar";
+            this.Text = "Encriptador/Desencriptador de Código de César";
+            this.grpOpcion.ResumeLayout(false);
+            this.grpOpcion.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltitulo;
+        private System.Windows.Forms.Label lbltexto;
+        private System.Windows.Forms.Label lblllave;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtLlave;
+        private System.Windows.Forms.TextBox txtTexto;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.GroupBox grpOpcion;
+        private System.Windows.Forms.RadioButton rbdEncriptar;
+        private System.Windows.Forms.RadioButton rbdDesencriptar;
+        private System.Windows.Forms.Button btnEncriptar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ encriptacion/encriptacion/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace encriptacion
+{
+    public partial class frmCesar : Form
+    {
+        public static Boolean IsNumeric(string valor)
+        {
+            int result;
+            return int.TryParse(valor, out result);
+        }
+        public frmCesar()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbdEncriptar.Checked == true)
+            {
+                btnEncriptar.Text = "Encriptar";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncriptar_Click(object sender, EventArgs e)
+        {
+            // Intentamos convertir el valor de txtLlave a un número entero
+            if (int.TryParse(txtLlave.Text, out int llave) && llave > 0)
+            {
+                txtResultado.Clear();
+                int ascii;
+
+                // Para cada carácter en el objeto txtTexto
+                foreach (char c in txtTexto.Text)
+                {
+                    // Verificamos si el usuario desea encriptar o desencriptar el texto
+                    if (rbdEncriptar.Checked)
+                    {
+                        // Convertimos el carácter extraído a su equivalente número ASCII y le sumamos la llave
+                        ascii = (int)c + llave;
+                    }
+                    else
+                    {
+                        // Convertimos el carácter extraído a su equivalente número ASCII y le restamos la llave
+                        ascii = (int)c - llave;
+                    }
+
+                    // Agregamos el carácter encriptado o desencriptado al resultado
+                    txtResultado.Text += (char)ascii;
+                }
+
+                lblResultado.Text = "Texto encriptado:";
+            }
+            else
+            {
+                MessageBox.Show("El dato introducido en la llave es incorrecto", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtLlave.Focus();
+                txtLlave.Clear();
+                txtLlave.BackColor = Color.Red;
+            }
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLlave_TextChanged(object sender, EventArgs e)
+        {
+            txtLlave.BackColor = Color.White;
+        }
+
+        private void rbdDesencriptar_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbdDesencriptar.Checked == true)
+            {
+                btnEncriptar.Text = "Desencriptar";
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtResultado.Clear();
+            txtTexto.Clear();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
