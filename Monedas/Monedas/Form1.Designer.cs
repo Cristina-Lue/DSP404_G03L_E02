d107d8563dc12f37b0a5911ba07092e013cc87d2
Cristina-Lue <aracelylue5@gmail.com>
2024-07-27 14:41:31 -0600

    Add files via upload

---
+++ Monedas/Monedas/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Monedas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.cmbMonedaOrigen = new System.Windows.Forms.ComboBox();
+            this.cmbMonedaDestino = new System.Windows.Forms.ComboBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnConvertir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(163, 61);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCantidad.TabIndex = 0;
+            // 
+            // cmbMonedaOrigen
+            // 
+            this.cmbMonedaOrigen.FormattingEnabled = true;
+            this.cmbMonedaOrigen.Location = new System.Drawing.Point(30, 132);
+            this.cmbMonedaOrigen.Name = "cmbMonedaOrigen";
+            this.cmbMonedaOrigen.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonedaOrigen.TabIndex = 1;
+            // 
+            // cmbMonedaDestino
+            // 
+            this.cmbMonedaDestino.FormattingEnabled = true;
+            this.cmbMonedaDestino.Location = new System.Drawing.Point(224, 132);
+            this.cmbMonedaDestino.Name = "cmbMonedaDestino";
+            this.cmbMonedaDestino.Size = new System.Drawing.Size(121, 21);
+            this.cmbMonedaDestino.TabIndex = 2;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(64, 214);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(55, 13);
+            this.lblResultado.TabIndex = 3;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // btnConvertir
+            // 
+            this.btnConvertir.Location = new System.Drawing.Point(397, 130);
+            this.btnConvertir.Name = "btnConvertir";
+            this.btnConvertir.Size = new System.Drawing.Size(75, 23);
+            this.btnConvertir.TabIndex = 4;
+            this.btnConvertir.Text = "Convertir";
+            this.btnConvertir.UseVisualStyleBackColor = true;
+            this.btnConvertir.Click += new System.EventHandler(this.btnConvertir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(160, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Conversor de monedas.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Cantidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tipo de Moneda";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Moneda a Convertir";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(521, 402);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvertir);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cmbMonedaDestino);
+            this.Controls.Add(this.cmbMonedaOrigen);
+            this.Controls.Add(this.txtCantidad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.ComboBox cmbMonedaOrigen;
+        private System.Windows.Forms.ComboBox cmbMonedaDestino;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnConvertir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Monedas/Monedas/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monedas
+{
+    public partial class Form1 : Form
+    {
+       
+               private Dictionary<string, double> tasasDeCambio = new Dictionary<string, double>
+    {
+        { "USD", 1.0 },
+        { "EUR", 0.85 },
+        { "GBP", 0.75 },
+        { "JPY", 110.0 },
+        { "AUD", 1.35 }
+    };
+
+        public Form1()
+        {
+            InitializeComponent();
+            CargarMonedas();
+        }
+
+        private void CargarMonedas()
+        {
+            cmbMonedaOrigen.Items.AddRange(tasasDeCambio.Keys.ToArray());
+            cmbMonedaDestino.Items.AddRange(tasasDeCambio.Keys.ToArray());
+            cmbMonedaOrigen.SelectedIndex = 0;
+            cmbMonedaDestino.SelectedIndex = 1;
+        }
+
+        private void btnConvertir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double cantidad = double.Parse(txtCantidad.Text);
+                string monedaOrigen = cmbMonedaOrigen.SelectedItem.ToString();
+                string monedaDestino = cmbMonedaDestino.SelectedItem.ToString();
+
+                double resultado = ConvertirMoneda(cantidad, monedaOrigen, monedaDestino);
+                lblResultado.Text = $"{cantidad} {monedaOrigen} = {resultado} {monedaDestino}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private double ConvertirMoneda(double cantidad, string monedaOrigen, string monedaDestino)
+        {
+            double tasaOrigen = tasasDeCambio[monedaOrigen];
+            double tasaDestino = tasasDeCambio[monedaDestino];
+            return cantidad * (tasaDestino / tasaOrigen);
+        }
+    }
+}
