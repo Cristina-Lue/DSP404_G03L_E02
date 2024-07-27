720ce6ba5f62a23e5f2a40ec3990f73f300918c6
Cristina-Lue <aracelylue5@gmail.com>
2024-07-26 18:06:06 -0600

    Add files via upload

---
+++ fibonacci/fibonacci/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace fibonacci
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.txtValorMaximo = new System.Windows.Forms.TextBox();
+            this.lstFibonacci = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnAceptar.Location = new System.Drawing.Point(304, 51);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(115, 23);
+            this.btnAceptar.TabIndex = 0;
+            this.btnAceptar.Text = "Confirmar número";
+            this.btnAceptar.UseVisualStyleBackColor = false;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnGenerar.Location = new System.Drawing.Point(304, 95);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(115, 23);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar serie";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // txtValorMaximo
+            // 
+            this.txtValorMaximo.Location = new System.Drawing.Point(138, 78);
+            this.txtValorMaximo.Name = "txtValorMaximo";
+            this.txtValorMaximo.Size = new System.Drawing.Size(100, 20);
+            this.txtValorMaximo.TabIndex = 2;
+            // 
+            // lstFibonacci
+            // 
+            this.lstFibonacci.FormattingEnabled = true;
+            this.lstFibonacci.Location = new System.Drawing.Point(167, 171);
+            this.lstFibonacci.Name = "lstFibonacci";
+            this.lstFibonacci.Size = new System.Drawing.Size(120, 121);
+            this.lstFibonacci.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(183, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Serie Fibonacci";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Introduzca un número";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 342);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstFibonacci);
+            this.Controls.Add(this.txtValorMaximo);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.btnAceptar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.TextBox txtValorMaximo;
+        private System.Windows.Forms.ListBox lstFibonacci;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ fibonacci/fibonacci/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fibonacci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            //obtener valor
+            if (int.TryParse(txtValorMaximo.Text, out int valorMaximo))
+            {
+                //Establecer el valor maximo y acticar boton
+
+                btnGenerar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Introduxca un valor númerico valido.");
+
+                btnGenerar.Enabled = false;
+            }
+            }
+    
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            lstFibonacci.Items.Clear();
+            if (int.TryParse(txtValorMaximo.Text, out int valorMaximo))
+            {
+                var serieFibonacci = GenerarFibonacci(valorMaximo);
+
+                foreach (var numero in serieFibonacci)
+                {
+                    lstFibonacci.Items.Add(numero);
+                }
+            }
+        }
+
+        private List<int> GenerarFibonacci(int valorMaximo)
+        {
+            var serie = new List<int>();
+
+            if (valorMaximo < 0)
+                return serie;
+
+            int a = 0, b = 1;
+            serie.Add(a);
+
+            while (b <= valorMaximo)
+            {
+                serie.Add(b);
+                int temp = a + b;
+                a = b;
+                b = temp;
+            }
+
+            return serie;
+        }
+    }
+}
