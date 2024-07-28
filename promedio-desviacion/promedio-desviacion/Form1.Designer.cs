4e17938dab905e6b55f4f7a7ba6da626baef9c9b
Cristina-Lue <aracelylue5@gmail.com>
2024-07-27 19:34:35 -0600

    Add files via upload

---
+++ promedio-desviacion/promedio-desviacion/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace promedio_desviacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtNumero4 = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblDesviacionTipica = new System.Windows.Forms.Label();
+            this.btnFin = new System.Windows.Forms.Button();
+            this.lblPromedio = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(91, 148);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Calculo de disviacion tipica y promedio";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.Location = new System.Drawing.Point(24, 107);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(85, 20);
+            this.txtNumero1.TabIndex = 2;
+            this.txtNumero1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtNumero4
+            // 
+            this.txtNumero4.Location = new System.Drawing.Point(358, 107);
+            this.txtNumero4.Name = "txtNumero4";
+            this.txtNumero4.Size = new System.Drawing.Size(85, 20);
+            this.txtNumero4.TabIndex = 3;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.Location = new System.Drawing.Point(135, 107);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(85, 20);
+            this.txtNumero2.TabIndex = 4;
+            // 
+            // txtNumero3
+            // 
+            this.txtNumero3.Location = new System.Drawing.Point(245, 107);
+            this.txtNumero3.Name = "txtNumero3";
+            this.txtNumero3.Size = new System.Drawing.Size(85, 20);
+            this.txtNumero3.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(163, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Introduzca cuatro números:";
+            // 
+            // lblDesviacionTipica
+            // 
+            this.lblDesviacionTipica.AutoSize = true;
+            this.lblDesviacionTipica.Location = new System.Drawing.Point(295, 208);
+            this.lblDesviacionTipica.Name = "lblDesviacionTipica";
+            this.lblDesviacionTipica.Size = new System.Drawing.Size(94, 13);
+            this.lblDesviacionTipica.TabIndex = 7;
+            this.lblDesviacionTipica.Text = "Desviación Típica";
+            // 
+            // btnFin
+            // 
+            this.btnFin.Location = new System.Drawing.Point(298, 148);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(75, 23);
+            this.btnFin.TabIndex = 8;
+            this.btnFin.Text = "Salir";
+            this.btnFin.UseVisualStyleBackColor = true;
+            this.btnFin.Click += new System.EventHandler(this.btnFin_Click);
+            // 
+            // lblPromedio
+            // 
+            this.lblPromedio.AutoSize = true;
+            this.lblPromedio.Location = new System.Drawing.Point(88, 208);
+            this.lblPromedio.Name = "lblPromedio";
+            this.lblPromedio.Size = new System.Drawing.Size(51, 13);
+            this.lblPromedio.TabIndex = 9;
+            this.lblPromedio.Text = "&Promedio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 295);
+            this.Controls.Add(this.lblPromedio);
+            this.Controls.Add(this.btnFin);
+            this.Controls.Add(this.lblDesviacionTipica);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtNumero3);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtNumero4);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtNumero4;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblDesviacionTipica;
+        private System.Windows.Forms.Button btnFin;
+        private System.Windows.Forms.Label lblPromedio;
+    }
+}
+
---
+++ promedio-desviacion/promedio-desviacion/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace promedio_desviacion
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Leer numeros
+                double num1 = double.Parse(txtNumero1.Text);
+                double num2 = double.Parse(txtNumero2.Text);
+                double num3 = double.Parse(txtNumero3.Text);
+                double num4 = double.Parse(txtNumero4.Text);
+
+                double[] numeros = { num1, num2, num3, num4 };
+
+                //Calculo del promedio
+
+                double promedio = numeros.Average();
+
+                //Calculo de desviacion tipica
+
+                double varianza = numeros.Average(v => Math.Pow(v - promedio, 2));
+                double desviacionTipica = Math.Pow(Math.Sqrt(varianza),2);
+
+                //Mostrar
+
+                lblPromedio.Text = "Promedio: " + promedio;
+
+                lblDesviacionTipica.Text = "Desviación Típica: " + desviacionTipica;
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Ingrese números válidos.", "Error de formato",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnFin_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
