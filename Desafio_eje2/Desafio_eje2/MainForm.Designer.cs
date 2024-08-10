0bf70f47c23e546324dad64a847ba50e7070400f
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2024-08-10 16:58:25 -0600

    Add files via upload

---
+++ Desafio_eje2/Desafio_eje2/MainForm.Designer.cs
@@ -0,0 +1,169 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: MINED
+ * Date: 09/08/2024
+ * Time: 19:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Desafio_eje2
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label lblMensaje;
+		private System.Windows.Forms.TextBox txtCuenta;
+		private System.Windows.Forms.TextBox txtCantidad;
+		private System.Windows.Forms.TextBox txtCuentaDestino;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnConfirmar;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// 
+		private void InitializeComponent()
+		{
+			this.button2 = new System.Windows.Forms.Button();
+			this.lblMensaje = new System.Windows.Forms.Label();
+			this.txtCuenta = new System.Windows.Forms.TextBox();
+			this.txtCantidad = new System.Windows.Forms.TextBox();
+			this.txtCuentaDestino = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnConfirmar = new System.Windows.Forms.Button();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(28, 22);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(114, 34);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Iniciar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// lblMensaje
+			// 
+			this.lblMensaje.Location = new System.Drawing.Point(340, 9);
+			this.lblMensaje.Name = "lblMensaje";
+			this.lblMensaje.Size = new System.Drawing.Size(323, 23);
+			this.lblMensaje.TabIndex = 2;
+			this.lblMensaje.Text = "Mensaje";
+			// 
+			// txtCuenta
+			// 
+			this.txtCuenta.Location = new System.Drawing.Point(374, 80);
+			this.txtCuenta.Name = "txtCuenta";
+			this.txtCuenta.Size = new System.Drawing.Size(100, 20);
+			this.txtCuenta.TabIndex = 4;
+			// 
+			// txtCantidad
+			// 
+			this.txtCantidad.Location = new System.Drawing.Point(374, 128);
+			this.txtCantidad.Name = "txtCantidad";
+			this.txtCantidad.Size = new System.Drawing.Size(100, 20);
+			this.txtCantidad.TabIndex = 6;
+			// 
+			// txtCuentaDestino
+			// 
+			this.txtCuentaDestino.Location = new System.Drawing.Point(374, 198);
+			this.txtCuentaDestino.Name = "txtCuentaDestino";
+			this.txtCuentaDestino.Size = new System.Drawing.Size(100, 20);
+			this.txtCuentaDestino.TabIndex = 7;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(49, 127);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(149, 21);
+			this.comboBox1.TabIndex = 8;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(374, 54);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(172, 23);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "ingrese el numero de cuenta";
+			// 
+			// btnConfirmar
+			// 
+			this.btnConfirmar.Location = new System.Drawing.Point(162, 22);
+			this.btnConfirmar.Name = "btnConfirmar";
+			this.btnConfirmar.Size = new System.Drawing.Size(114, 34);
+			this.btnConfirmar.TabIndex = 11;
+			this.btnConfirmar.Text = "confirmar";
+			this.btnConfirmar.UseVisualStyleBackColor = true;
+			this.btnConfirmar.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(374, 103);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 12;
+			this.label2.Text = "ingrese la cantidad";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(374, 172);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 13;
+			this.label3.Text = "ingrese la cuenta de destino";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.ClientSize = new System.Drawing.Size(665, 304);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.btnConfirmar);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.txtCuentaDestino);
+			this.Controls.Add(this.txtCantidad);
+			this.Controls.Add(this.txtCuenta);
+			this.Controls.Add(this.lblMensaje);
+			this.Controls.Add(this.button2);
+			this.Name = "MainForm";
+			this.Text = "Desafio_eje2";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		// 
+			
+		
+		}
+	}
+}  
---
+++ Desafio_eje2/Desafio_eje2/MainForm.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Desafio_eje2
+{
+    public partial class MainForm : Form
+    {
+        private int[] cuentas = { 1000001, 1000002, 1000004 };
+        private decimal[] saldos = { 400m, 200m, 500m };
+
+        public MainForm()
+        {
+        	InitializeComponent();
+            comboBox1.Items.Add("Consulta de saldo");
+            comboBox1.Items.Add("Retiro de fondos");
+            comboBox1.Items.Add("Consignaciones");
+            comboBox1.Items.Add("Transferencias");
+            comboBox1.Items.Add("Salir");
+            
+        }
+
+        private void Button2Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int cuenta = int.Parse(txtCuenta.Text);
+                int indice = Array.IndexOf(cuentas, cuenta);
+                if (indice >= 0)
+                {
+                    lblMensaje.Text = "¡Bienvenido!";
+                    MostrarMenu(indice);
+                }
+                else
+                {
+                    lblMensaje.Text = "Cuenta no encontrada.";
+                }
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "El número de cuenta debe ser un valor numérico.";
+            }
+        }
+
+        private void MostrarMenu(int indice)
+        {
+            
+        }
+
+        private void Button1Click(object sender, EventArgs e)
+        {
+        	
+        	
+            try
+            {
+                int opcion = comboBox1.SelectedIndex + 1; 
+                int cuenta = int.Parse(txtCuenta.Text);
+                int indice = Array.IndexOf(cuentas, cuenta);
+
+                switch (opcion)
+                {
+                    case 1:
+                		lblMensaje.Text = "El saldo actual de la cuenta 	"+	cuenta +"	es de	"+		saldos[indice];
+                        break;
+                    case 2:
+                        RetirarFondos(indice);
+                        break;
+                    case 3:
+                        ConsignarFondos(indice);
+                        break;
+                    case 4:
+                        TransferirFondos(indice);
+                        break;
+                    case 5:
+                        lblMensaje.Text = "Gracias por usar el cajero automático.";
+                        Application.Exit();
+                        break;
+                    default:
+                        lblMensaje.Text = "Opción no válida.";
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "Por favor, seleccione una opción válida.";
+            }
+        }
+
+        private void RetirarFondos(int indice)
+        {
+            try
+            {
+                decimal cantidad = decimal.Parse(txtCantidad.Text);
+                if (saldos[indice] >= cantidad)
+                {
+                    saldos[indice] -= cantidad;
+                    lblMensaje.Text = "Has retirado"+cantidad+ "El saldo actual es"+saldos[indice];
+                }
+                else
+                {
+                    lblMensaje.Text = "Fondos insuficientes.";
+                }
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "La cantidad debe ser un valor numérico.";
+            }
+        }
+
+        private void ConsignarFondos(int indice)
+        {
+            try
+            {
+                decimal cantidad = decimal.Parse(txtCantidad.Text);
+                saldos[indice] += cantidad;
+                lblMensaje.Text = "Has consignado"+cantidad+" El saldo actual es"+saldos[indice];
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "La cantidad debe ser un valor numérico.";
+            }
+        }
+
+        private void TransferirFondos(int indice)
+        {
+            try
+            {
+                int cuentaDestino = int.Parse(txtCuentaDestino.Text);
+                int indiceDestino = Array.IndexOf(cuentas, cuentaDestino);
+                decimal cantidad = decimal.Parse(txtCantidad.Text);
+
+                if (indiceDestino >= 0 && saldos[indice] >= cantidad)
+                {
+                    saldos[indice] -= cantidad;
+                    saldos[indiceDestino] += cantidad;
+                    lblMensaje.Text = "Has transferido"+cantidad + "a la cuenta"+cuentaDestino+" El saldo actual es"+saldos[indice];
+                }
+                else
+                {
+                    lblMensaje.Text = "Fondos insuficientes o cuenta destino no encontrada.";
+                }
+            }
+            catch (FormatException)
+            {
+                lblMensaje.Text = "La cantidad y el número de cuenta destino deben ser valores numéricos.";
+            }
+        }
+    }
+}
