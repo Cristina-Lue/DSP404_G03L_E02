cfcadd52498c4d3335efc5b7ef8a0ecfdb6b669d
AlejandraHernandez1306 <hm240103@alumno.udb.edu.sv>
2024-08-11 22:02:22 -0600

    Add files via upload

---
+++ Guia1/Ejemplo_3/Ejercicio_3/Ejercicio_3/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Ejercicio_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblApe1 = new System.Windows.Forms.Label();
+            this.lblApe2 = new System.Windows.Forms.Label();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblCUM = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtCUM = new System.Windows.Forms.TextBox();
+            this.btnAnalisis = new System.Windows.Forms.Button();
+            this.btnFin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblApe1
+            // 
+            this.lblApe1.AutoSize = true;
+            this.lblApe1.Location = new System.Drawing.Point(93, 47);
+            this.lblApe1.Name = "lblApe1";
+            this.lblApe1.Size = new System.Drawing.Size(82, 16);
+            this.lblApe1.TabIndex = 0;
+            this.lblApe1.Text = "1er Apellido:";
+            // 
+            // lblApe2
+            // 
+            this.lblApe2.AutoSize = true;
+            this.lblApe2.Location = new System.Drawing.Point(93, 102);
+            this.lblApe2.Name = "lblApe2";
+            this.lblApe2.Size = new System.Drawing.Size(86, 16);
+            this.lblApe2.TabIndex = 1;
+            this.lblApe2.Text = "2do Apellido:";
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(93, 165);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(66, 16);
+            this.lblNom.TabIndex = 2;
+            this.lblNom.Text = "Nombres:";
+            // 
+            // lblCUM
+            // 
+            this.lblCUM.AutoSize = true;
+            this.lblCUM.Location = new System.Drawing.Point(93, 225);
+            this.lblCUM.Name = "lblCUM";
+            this.lblCUM.Size = new System.Drawing.Size(94, 16);
+            this.lblCUM.TabIndex = 3;
+            this.lblCUM.Text = "Valor de CUM:";
+            this.lblCUM.Click += new System.EventHandler(this.lblCUM_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(544, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(189, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(544, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(189, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(544, 22);
+            this.textBox3.TabIndex = 6;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(189, 290);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(544, 22);
+            this.txtResult.TabIndex = 7;
+            // 
+            // txtCUM
+            // 
+            this.txtCUM.Location = new System.Drawing.Point(189, 222);
+            this.txtCUM.Name = "txtCUM";
+            this.txtCUM.Size = new System.Drawing.Size(544, 22);
+            this.txtCUM.TabIndex = 8;
+            this.txtCUM.Text = "0.0";
+            // 
+            // btnAnalisis
+            // 
+            this.btnAnalisis.Location = new System.Drawing.Point(163, 367);
+            this.btnAnalisis.Name = "btnAnalisis";
+            this.btnAnalisis.Size = new System.Drawing.Size(180, 23);
+            this.btnAnalisis.TabIndex = 9;
+            this.btnAnalisis.Text = "Calcular UV";
+            this.btnAnalisis.UseVisualStyleBackColor = true;
+            this.btnAnalisis.Click += new System.EventHandler(this.btnAnalisis_Click);
+            // 
+            // btnFin
+            // 
+            this.btnFin.Location = new System.Drawing.Point(398, 367);
+            this.btnFin.Name = "btnFin";
+            this.btnFin.Size = new System.Drawing.Size(198, 23);
+            this.btnFin.TabIndex = 10;
+            this.btnFin.Text = "Salir del Programa";
+            this.btnFin.UseVisualStyleBackColor = true;
+            this.btnFin.Click += new System.EventHandler(this.btnFin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnFin);
+            this.Controls.Add(this.btnAnalisis);
+            this.Controls.Add(this.txtCUM);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblCUM);
+            this.Controls.Add(this.lblNom);
+            this.Controls.Add(this.lblApe2);
+            this.Controls.Add(this.lblApe1);
+            this.Name = "Form1";
+            this.Text = "Resultado de evaluación";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblApe1;
+        private System.Windows.Forms.Label lblApe2;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblCUM;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtCUM;
+        private System.Windows.Forms.Button btnAnalisis;
+        private System.Windows.Forms.Button btnFin;
+    }
+}
+
---
+++ Guia1/Ejemplo_3/Ejercicio_3/Ejercicio_3/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_3
+{
+    public partial class Form1 : Form
+    {
+        private string noms, ape1, ape2;
+        private double CUM;
+        private int UV;
+
+        // Validacion de los datos recibidos son números
+        public static bool IsNumeric(string valor)
+        {
+            int result;
+            return int.TryParse(valor, out result);
+        }
+
+        // Verifica el CUM
+        private void EvaluarCUM()
+        {
+            string nombrecompleto = noms + " " + ape1 + " " + ape2;
+            nombrecompleto = nombrecompleto.ToUpper();
+
+            if (CUM < 0 || CUM > 10)
+            {
+                MessageBox.Show("Valor de CUM fuera del rango establecido (0.0 - 10.0)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtCUM.Focus();
+            }
+            else
+            {
+                //Usa estructura switch 
+                switch (Convert.ToInt32(CUM))
+                {
+                    case 8:
+                    case 9:
+                    case 10:
+                        UV = 32;
+                        break;
+                    case 7:
+                        UV = 24;
+                        break;
+                    case 6:
+                        UV = 20;
+                        break;
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                    case 5:
+                        UV = 16;
+                        break;
+                    default:
+                        UV = 0;
+                        break;
+                }
+
+                txtResult.Text = nombrecompleto + " puede cursar " + UV + " UV";
+            }
+        }
+
+        private void btnAnalisis_Click(object sender, EventArgs e)
+        {
+            if (IsNumeric(txtResult.Text))
+            {
+                CUM = double.Parse(txtResult.Text);
+                EvaluarCUM();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un valor numérico válido para poder calcular el CUM", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtResult.Focus();
+            }
+        }
+        private void btnFin_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void lblCUM_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
