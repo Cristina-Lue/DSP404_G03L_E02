b76b287437d7484069ef4e32f28786075d4a0012
AlejandraHernandez1306 <hm240103@alumno.udb.edu.sv>
2024-08-11 22:07:28 -0600

    Add files via upload

---
+++ Guia2/Ejercicio_3/Guia2_Ejercicio3/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Guia2_Ejercicio3
+{
+    partial class frmReves
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNumero = new Label();
+            txtNumero = new TextBox();
+            txtReves = new TextBox();
+            lblReves = new Label();
+            btnCalcular = new Button();
+            btnSalir = new Button();
+            btnLimpiar = new Button();
+            SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(92, 49);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(66, 20);
+            lblNumero.TabIndex = 0;
+            lblNumero.Text = "Número:";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(92, 89);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(615, 27);
+            txtNumero.TabIndex = 1;
+            // 
+            // txtReves
+            // 
+            txtReves.Location = new Point(92, 206);
+            txtReves.Name = "txtReves";
+            txtReves.Size = new Size(615, 27);
+            txtReves.TabIndex = 2;
+            // 
+            // lblReves
+            // 
+            lblReves.AutoSize = true;
+            lblReves.Location = new Point(92, 158);
+            lblReves.Name = "lblReves";
+            lblReves.Size = new Size(64, 20);
+            lblReves.TabIndex = 3;
+            lblReves.Text = "Al reves:";
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(201, 324);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(94, 29);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "&Calcular ";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(511, 324);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(94, 29);
+            btnSalir.TabIndex = 5;
+            btnSalir.Text = "&Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(355, 324);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(94, 29);
+            btnLimpiar.TabIndex = 6;
+            btnLimpiar.Text = "&Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // frmReves
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnSalir);
+            Controls.Add(btnCalcular);
+            Controls.Add(lblReves);
+            Controls.Add(txtReves);
+            Controls.Add(txtNumero);
+            Controls.Add(lblNumero);
+            Name = "frmReves";
+            Text = "Mostrar un número al reves";
+            Load += frmReves_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNumero;
+        private TextBox txtNumero;
+        private TextBox txtReves;
+        private Label lblReves;
+        private Button btnCalcular;
+        private Button btnSalir;
+        private Button btnLimpiar;
+    }
+}
---
+++ Guia2/Ejercicio_4/Guia2_Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Guia2_Ejercicio4
+{
+    partial class frmULAM
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNumero = new Label();
+            label2 = new Label();
+            lstLista = new ListBox();
+            txtNumero = new TextBox();
+            btnCalcular = new Button();
+            btnLimpiar = new Button();
+            btnFin = new Button();
+            SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            lblNumero.AutoSize = true;
+            lblNumero.Location = new Point(452, 104);
+            lblNumero.Name = "lblNumero";
+            lblNumero.Size = new Size(132, 20);
+            lblNumero.TabIndex = 0;
+            lblNumero.Text = "Suseción de ULAM";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(113, 43);
+            label2.Name = "label2";
+            label2.Size = new Size(192, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ingrese un número positivo:";
+            // 
+            // lstLista
+            // 
+            lstLista.FormattingEnabled = true;
+            lstLista.Location = new Point(311, 140);
+            lstLista.Name = "lstLista";
+            lstLista.Size = new Size(397, 164);
+            lstLista.TabIndex = 2;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(311, 43);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(397, 27);
+            txtNumero.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(113, 140);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(94, 29);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "&Calcular ";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(111, 205);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(94, 29);
+            btnLimpiar.TabIndex = 5;
+            btnLimpiar.Text = "&Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click;
+            // 
+            // btnFin
+            // 
+            btnFin.Location = new Point(113, 276);
+            btnFin.Name = "btnFin";
+            btnFin.Size = new Size(94, 29);
+            btnFin.TabIndex = 6;
+            btnFin.Text = "&Salir";
+            btnFin.UseVisualStyleBackColor = true;
+            btnFin.Click += btnFin_Click;
+            // 
+            // frmULAM
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnFin);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtNumero);
+            Controls.Add(lstLista);
+            Controls.Add(label2);
+            Controls.Add(lblNumero);
+            Name = "frmULAM";
+            Text = "Suseción de ULAM";
+            Load += frmULAM_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNumero;
+        private Label label2;
+        private ListBox lstLista;
+        private TextBox txtNumero;
+        private Button btnCalcular;
+        private Button btnLimpiar;
+        private Button btnFin;
+    }
+}
