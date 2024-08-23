de2b04c01a4b0eb06d34b796e98bcc46dba7a4ec
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2024-08-23 14:20:15 -0600

    Add files via upload

---
+++ DESAFIO/Desafío1/Eje3/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace TaskManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeleteTask = new System.Windows.Forms.Button();
+            this.btnEditTask = new System.Windows.Forms.Button();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.txtTaskName = new System.Windows.Forms.TextBox();
+            this.btnCompleteTask = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.lstCompletedTasks = new System.Windows.Forms.ListView();
+            this.btnAddTask = new System.Windows.Forms.Button();
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FechaLimite = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NombreTarea = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.FechaEntrega = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnDeleteTask
+            // 
+            this.btnDeleteTask.Location = new System.Drawing.Point(575, 57);
+            this.btnDeleteTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnDeleteTask.Name = "btnDeleteTask";
+            this.btnDeleteTask.Size = new System.Drawing.Size(100, 43);
+            this.btnDeleteTask.TabIndex = 1;
+            this.btnDeleteTask.Text = "Eliminar Tarea";
+            this.btnDeleteTask.UseVisualStyleBackColor = true;
+            this.btnDeleteTask.Click += new System.EventHandler(this.btnDeleteTask_Click);
+            // 
+            // btnEditTask
+            // 
+            this.btnEditTask.Location = new System.Drawing.Point(457, 107);
+            this.btnEditTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEditTask.Name = "btnEditTask";
+            this.btnEditTask.Size = new System.Drawing.Size(100, 41);
+            this.btnEditTask.TabIndex = 2;
+            this.btnEditTask.Text = "Editar Tarea";
+            this.btnEditTask.UseVisualStyleBackColor = true;
+            this.btnEditTask.Click += new System.EventHandler(this.btnEditTask_Click);
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Location = new System.Drawing.Point(99, 165);
+            this.dtpDueDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(265, 22);
+            this.dtpDueDate.TabIndex = 5;
+            // 
+            // txtTaskName
+            // 
+            this.txtTaskName.Location = new System.Drawing.Point(205, 66);
+            this.txtTaskName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTaskName.Name = "txtTaskName";
+            this.txtTaskName.Size = new System.Drawing.Size(132, 22);
+            this.txtTaskName.TabIndex = 6;
+            // 
+            // btnCompleteTask
+            // 
+            this.btnCompleteTask.Location = new System.Drawing.Point(575, 107);
+            this.btnCompleteTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCompleteTask.Name = "btnCompleteTask";
+            this.btnCompleteTask.Size = new System.Drawing.Size(100, 58);
+            this.btnCompleteTask.TabIndex = 7;
+            this.btnCompleteTask.Text = "Marcar como completada";
+            this.btnCompleteTask.UseVisualStyleBackColor = true;
+            this.btnCompleteTask.Click += new System.EventHandler(this.btnCompleteTask_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(96, 70);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Ingresar tarea:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(96, 128);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(196, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Ingrese la fecha de vencimiento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(153, 242);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Tareas Pendientes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(453, 242);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tareas Completadas:";
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nombre,
+            this.FechaLimite});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(157, 311);
+            this.listView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(121, 98);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // lstCompletedTasks
+            // 
+            this.lstCompletedTasks.CheckBoxes = true;
+            this.lstCompletedTasks.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.NombreTarea,
+            this.FechaEntrega});
+            this.lstCompletedTasks.FullRowSelect = true;
+            this.lstCompletedTasks.GridLines = true;
+            this.lstCompletedTasks.HideSelection = false;
+            this.lstCompletedTasks.Location = new System.Drawing.Point(457, 311);
+            this.lstCompletedTasks.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lstCompletedTasks.Name = "lstCompletedTasks";
+            this.lstCompletedTasks.Size = new System.Drawing.Size(121, 98);
+            this.lstCompletedTasks.TabIndex = 13;
+            this.lstCompletedTasks.UseCompatibleStateImageBehavior = false;
+            this.lstCompletedTasks.View = System.Windows.Forms.View.Details;
+            this.lstCompletedTasks.SelectedIndexChanged += new System.EventHandler(this.lstCompletedTasks_SelectedIndexChanged);
+            // 
+            // btnAddTask
+            // 
+            this.btnAddTask.Location = new System.Drawing.Point(457, 57);
+            this.btnAddTask.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddTask.Name = "btnAddTask";
+            this.btnAddTask.Size = new System.Drawing.Size(100, 43);
+            this.btnAddTask.TabIndex = 14;
+            this.btnAddTask.Text = "Agregar tarea";
+            this.btnAddTask.UseVisualStyleBackColor = true;
+            this.btnAddTask.Click += new System.EventHandler(this.btnAddTask_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            // 
+            // FechaLimite
+            // 
+            this.FechaLimite.Text = "FechaLimite";
+            // 
+            // NombreTarea
+            // 
+            this.NombreTarea.Text = "Nombre";
+            // 
+            // FechaEntrega
+            // 
+            this.FechaEntrega.Text = "FechaLímite";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(764, 554);
+            this.Controls.Add(this.btnAddTask);
+            this.Controls.Add(this.lstCompletedTasks);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCompleteTask);
+            this.Controls.Add(this.txtTaskName);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.btnEditTask);
+            this.Controls.Add(this.btnDeleteTask);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDeleteTask;
+        private System.Windows.Forms.Button btnEditTask;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.TextBox txtTaskName;
+        private System.Windows.Forms.Button btnCompleteTask;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lstCompletedTasks;
+        private System.Windows.Forms.Button btnAddTask;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader FechaLimite;
+        private System.Windows.Forms.ColumnHeader NombreTarea;
+        private System.Windows.Forms.ColumnHeader FechaEntrega;
+    }
+}
+
