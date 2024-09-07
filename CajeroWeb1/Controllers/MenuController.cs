a1b37747ffb3d41643dad296581c96ec878303dc
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2024-09-07 11:06:04 -0600

    Add files via upload

---
+++ CajeroWeb1/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroWeb1.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index(string sNumeroTarjeta, double sSaldo)
+        {
+            ViewBag.sNumeroTarjeta = sNumeroTarjeta;
+            ViewBag.sSaldo = sSaldo;
+            return View();
+        }
+    }
+}
