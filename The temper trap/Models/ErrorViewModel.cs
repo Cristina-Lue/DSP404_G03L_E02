58a005f37e12daff63d55623161708d436ade30a
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2024-09-25 16:21:18 -0600

    Delete The temper trap directory

--- The temper trap/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace The_Temper_Trap.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
