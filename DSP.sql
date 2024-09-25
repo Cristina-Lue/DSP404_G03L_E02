b5b4d2af4e1d23ce95ac5119e9e142e4c7d04145
Levi_PM241595 <151584999+most-improve123@users.noreply.github.com>
2024-09-24 19:44:27 -0600

    Add files via upload

---
+++ DSP.sql
@@ -0,0 +1,13 @@
+
+
+SELECT*FROM Clientes
+
+
+INSERT INTO Clientes (ClienteID, Nombre, Direccion, Telefono, CorreoElectronico, RestauranteID,Usuario, Contraseña)
+VALUES ('1', 'Carlos Mont', 'San Salvador', '78093543',  'hey100206@gmail.com', '22','Hola', 123456)
+
+
+SELECT*FROM Restaurante
+
+INSERT INTO Restaurante (RestauranteID, Nombre, Direccion, Telefono, Horario, TipoCocina)
+VALUES ('22', 'Carlos Mont', 'San Salvador', '78093543',  'Lunes a Viernes', 'Calidad')
