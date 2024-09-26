efd099baf2f5062c6230e510a26cad4ca6147903
PL240092 <aracelylue5@gmail.com>
2024-09-25 21:29:19 -0600

    Add files via upload

---
+++ The Temper Trap/The-Temper-Trap/Controllers/CarritoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class CarritoController : Controller
+{
+    [HttpPost]
+    public IActionResult AgregarAlCarrito(int idProducto)
+    {
+        // Lógica para agregar el producto al carrito
+
+        // Redirigir a la vista VerCarrito
+        return RedirectToAction("VerCarrito", "Carrito");
+    }
+
+    public IActionResult VerCarrito()
+    {
+        // Lógica para obtener el carrito y pasarlo a la vista
+        return View();
+    }
+}
